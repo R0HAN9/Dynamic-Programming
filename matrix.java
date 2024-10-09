6b429a39745d8cc18e6e44d8c43048858e11b610
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-09 11:03:34 +0530

    Update matrix.java

--- matrix.java
+++ matrix.java
@@ -109,3 +109,29 @@ class Solution {
         return result;
     }
 }
+
+
+// **LEETCODE DALILY**  Minimum Add to Make Parentheses Valid
+
+
+class Solution {
+    public int minAddToMakeValid(String s) {
+        
+        int open = 0;
+        int misMatch = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                open++;
+            }
+            else if (open > 0) {
+                open--;
+            }
+            else {
+                misMatch++;
+            }
+        }
+
+        return open + misMatch;
+    }
+}

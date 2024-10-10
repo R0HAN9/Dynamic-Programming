8b6d7563b6243d71c5298cbbebbc8c743fc59224
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-10 10:08:24 +0530

    Update FibonacciSequence.java

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -16,3 +16,22 @@ class Solution {
         return Math.min(dp[n - 1], dp[n - 2]);
     }
 }
+
+// 2. Fibonacci Number
+
+class Solution {
+    public int fib(int n) {
+        
+        if (n <= 1) return n;
+        int[] fibNum = new int[n + 1];
+
+        fibNum[0] = 1;
+        fibNum[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibNum[i] = fibNum[i - 1] + fibNum[i - 2];
+        }
+
+        return fibNum[n - 1];
+    }
+}

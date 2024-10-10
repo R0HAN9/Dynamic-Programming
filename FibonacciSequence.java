3c9edcc0f661e1bf8560ad1502730f749d57ca78
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-10 10:19:39 +0530

    Update FibonacciSequence.java

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -35,3 +35,30 @@ class Solution {
         return fibNum[n - 1];
     }
 }
+
+
+// LEETCODE DAILY
+
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty() || nums[stack.peek()] > nums[i]) {
+                stack.push(i);
+            }
+        }
+
+        int answer = 0;
+        for (int i = n-1; i > 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
+
+                answer = Math.max(answer, i - stack.peek());
+                stack.pop();
+            }
+        }
+        return answer;
+    }
+}
